54c35e9901b2ebe710c49c1c3f6520e6a3be6356
Johana Carrizo <johanacarrizo20@gmail.com>
2024-05-20 11:15:20 -0300

    Feat: create entities

---
+++ backend/src/main/java/com/backend/gg/entity/OrderDetail.java
@@ -0,0 +1,8 @@
+package com.backend.gg.entity;
+
+public class OrderDetail {
+
+    private Long id;
+    private int quantity;
+    private Product product;
+}
---
+++ backend/src/main/java/com/backend/gg/security/entity/User.java
@@ -0,0 +1,12 @@
+package com.backend.gg.security.entity;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String password;
+}
