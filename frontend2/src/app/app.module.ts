eb1987e7eb78b705cc679846880c463895997453
ZeckZero <109942589+ZeckZero@users.noreply.github.com>
2024-06-04 19:58:53 -0300

    frontend2

--- frontend2/src/app/app.module.ts
+++ frontend2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/core/main/main.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
