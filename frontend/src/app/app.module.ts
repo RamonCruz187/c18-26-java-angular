7e18edcdeab75dc65cc83724454f9537b14eadb7
ZeckZero <109942589+ZeckZero@users.noreply.github.com>
2024-06-04 13:18:57 -0300

    Main- footer- navBar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/core/main/main.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
