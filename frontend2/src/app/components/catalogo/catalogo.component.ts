e88ed325c8e0b67d4d8eae547f3d10a08cc78211
Virgi D <virginiadanitz@gmail.com>
2024-06-07 14:27:04 -0300

    cambios

--- frontend2/src/app/components/catalogo/catalogo.component.ts
+++ frontend2/src/app/components/catalogo/catalogo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpServiceService } from 'src/app/services/http-service.service';
 
 @Component({
   selector: 'app-catalogo',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent {
+  product: any;
 
+  constructor(private route: ActivatedRoute, private data:HttpServiceService) { }
+
+  ngOnInit(): void {
+    // Recuperar el ID del producto de la ruta
+    const categoria = this.route.snapshot.paramMap.get('categoria');
+    console.log(categoria);
+    this.data.getCategory(categoria).subscribe(data => {
+      console.log(data);
+  });
+  }
 }
